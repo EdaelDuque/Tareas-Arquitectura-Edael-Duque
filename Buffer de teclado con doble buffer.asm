d5b55fc05bcba548eb9ded8bfc2b3665ba416fd0
Edael Duque <e.duque1212@gmail.com>
2026-03-22 19:41:52 -0400

    Increase buffer sizes and improve messages

--- Buffer de teclado con doble buffer.asm
+++ Buffer de teclado con doble buffer.asm
@@ -1,15 +1,17 @@
 .data
-    bufA:  .space 50
-    bufB:  .space 50
+    bufA:  .space 100
+    bufB:  .space 100
     headA: .word 0
     headB: .word 0
-    flag:  .word 0          # 0 = Escribir en A, 1 = Escribir en B
-    msgA:  .asciiz "\nVaciando Buffer A: "
-    msgB:  .asciiz "\nVaciando Buffer B: "
+    flag:  .word 0 
+    msgA:  .asciiz "\n--- Vaciando Buffer A (Activo B) ---\n"
+    msgB:  .asciiz "\n--- Vaciando Buffer B (Activo A) ---\n"
 
 .text
+.globl main
+
 main:
-    # Habilitar teclado
+
     li $t0, 0xFFFF0000
     li $t1, 2
     sw $t1, 0($t0)
@@ -18,6 +20,7 @@ main:
     mtc0 $a0, $12
 
 loop_pingpong:
+
     li $v0, 30
     syscall
     move $s0, $a0
@@ -28,54 +31,88 @@ esperar_10s:
     subu $t0, $a0, $s0
     bltu $t0, 10000, esperar_10s
 
-    # Cambiar bandera y vaciar el anterior
     lw $t0, flag
-    xori $t0, $t0, 1    # Alternar 0 <-> 1
-    sw $t0, flag
+    beq $t0, $zero, cambiar_a_B
 
-    beq $t0, 1, imprimir_A
-    # Si ahora la bandera es 0, significa que acabamos de llenar el B
+    li $t1, 0
+    sw $t1, flag
     li $v0, 4
     la $a0, msgB
     syscall
     jal vaciar_B
     j loop_pingpong
 
-imprimir_A:
+cambiar_a_B:
+
+    li $t1, 1
+    sw $t1, flag
     li $v0, 4
     la $a0, msgA
     syscall
     jal vaciar_A
     j loop_pingpong
 
-# Funciones de vaciado omitidas por brevedad (similares al Ej 1)
-vaciar_A: sw $zero, headA; jr $ra
-vaciar_B: sw $zero, headB; jr $ra
+#Funciones de vaciado
+vaciar_A:
+    la $t2, bufA
+    lw $t3, headA
+    li $t4, 0
+loop_A:
+    beq $t4, $t3, fin_A
+    addu $t5, $t2, $t4
+    lb $a0, 0($t5)
+    li $v0, 11
+    syscall
+    addi $t4, $t4, 1
+    j loop_A
+fin_A:
+    sw $zero, headA
+    jr $ra
 
-# --- MANEJADOR DE INTERRUPCIONES ---
+vaciar_B:
+    la $t2, bufB
+    lw $t3, headB
+    li $t4, 0
+loop_B:
+    beq $t4, $t3, fin_B
+    addu $t5, $t2, $t4
+    lb $a0, 0($t5)
+    li $v0, 11
+    syscall
+    addi $t4, $t4, 1
+    j loop_B
+fin_B:
+    sw $zero, headB
+    jr $ra
+
+#Manejo de interrupciones
 .ktext 0x80000180
-    li $t0, 0xFFFF0004
-    lb $k0, 0($t0)      # Leer tecla
+    li $k0, 0xFFFF0004
+    lb $k1, 0($k0)
 
-    lw $k1, flag
-    beq $k1, 1, a_buffer_B
+    #Filtro Mayúsculas
+    blt $k1, 65, salir_int
+    bgt $k1, 90, salir_int
 
-    # Guardar en A
-    la $t1, bufA
-    lw $t2, headA
-    addu $t3, $t1, $t2
-    sb $k0, 0($t3)
-    addi $t2, $t2, 1
-    sw $t2, headA
+    lw $t8, flag
+    beq $t8, 1, guardar_en_B
+
+guardar_en_A:
+    la $t9, bufA
+    lw $t8, headA
+    addu $t7, $t9, $t8
+    sb $k1, 0($t7)
+    addi $t8, $t8, 1
+    sw $t8, headA
     j salir_int
 
-a_buffer_B:
-    la $t1, bufB
-    lw $t2, headB
-    addu $t3, $t1, $t2
-    sb $k0, 0($t3)
-    addi $t2, $t2, 1
-    sw $t2, headB
+guardar_en_B:
+    la $t9, bufB
+    lw $t8, headB
+    addu $t7, $t9, $t8
+    sb $k1, 0($t7)
+    addi $t8, $t8, 1
+    sw $t8, headB
 
 salir_int:
     eret
